0a453f0884d5f90d632c96fe2971eaf63bd2ba0d
Tyler Ferguson <tferguson@scottlogic.co.uk>
2014-11-25 17:06:43 +0000

    Added insight simple column chart with hard coded data

--- app/charts/main.controller.js
+++ app/charts/main.controller.js
@@ -7,11 +7,25 @@
  * # MainCtrl
  * Controller of the insightExplorer
  */
-angular.module('insightExplorer')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+angular.module('insightExplorer').controller('MainCtrl', function () {
+    
+    var data = [
+        { 'name': 'Hancock Campbell', 'age': 39, 'broticity': 78, 'eyeColor': 'blue' },
+        { 'name': 'Sybil Nielsen', 'age': 27, 'broticity': 49, 'eyeColor': 'green' },
+        { 'name': 'Pierce Rice', 'age': 29, 'broticity': 53, 'eyeColor': 'green' },
+        { 'name': 'Ferguson Dotson', 'age': 34, 'broticity': 15, 'eyeColor': 'green' },
+        { 'name': 'Alicia Byrd', 'age': 33, 'broticity': 54, 'eyeColor': 'blue' },
+        { 'name': 'Dean Fisher', 'age': 23, 'broticity': 64,'eyeColor': 'green' },
+        { 'name': 'Mckinney Harvey', 'age': 31, 'broticity': 23, 'eyeColor': 'blue' },
+        { 'name': 'Lakisha Battle', 'age': 33, 'broticity': 19, 'eyeColor': 'brown' },
+        { 'name': 'Levine Franklin', 'age': 37, 'broticity': 78, 'eyeColor': 'blue' },
+        { 'name': 'Kathrine Lewis', 'age': 27, 'broticity': 80, 'eyeColor': 'blue' },
+        { 'name': 'Jefferson Everett', 'age': 34, 'broticity': 59, 'eyeColor': 'brown' },
+        { 'name': 'Moss Vasquez', 'age': 39, 'broticity': 3, 'eyeColor': 'green' }
     ];
+    
+    var chart = new insight.SimpleColumnChart(data, '#chart', 'name', 'age')
+        .build();
+
+    chart.draw();
   });
