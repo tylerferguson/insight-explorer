66df7592112503c9cf3851694a4c54a7b416b905
Tyler Ferguson <tferguson@scottlogic.co.uk>
2014-12-10 11:15:08 +0000

    Added tests ensuring that selected properties passed to chart are tracked correctly after selection/ deselection

--- test/unit/controllers/sidebar.controller.js
+++ test/unit/controllers/sidebar.controller.js
@@ -24,13 +24,77 @@ describe('Controller: SideCtrl', function () {
 
     it('should keep track of properties selected by the user for charting', function() {
 
-        scope.setProperty('name');
-        scope.setProperty('age');
-        scope.setProperty('nationality');
+        scope.select(1, 'name');
+        expect(scope.dataProperties).toEqual(['name']);
 
+        scope.select(0, 'age');
+        expect(scope.dataProperties).toEqual(['name', 'age']);
+
+        scope.select(3, 'nationality');
         expect(scope.dataProperties).toEqual(['name', 'age', 'nationality']);
     });
 
+    it('should forget the last property selected by the user if clicked again', function() {
+
+        //Properties are initially selected
+        scope.select(1, 'name');
+        scope.select(0, 'age');
+        scope.select(3, 'nationality');
+
+        //Property is deselected by re-clicking
+        scope.select(3, 'nationality');
+
+        expect(scope.dataProperties).toEqual(['name', 'age']);
+    });
+
+    it('should keep track of properties that have been chosen after deselection', function() {
+
+        //Properties are initially selected
+        scope.select(1, 'name');
+        scope.select(0, 'age');
+        scope.select(3, 'nationality');
+
+        //Property is deselected by re-clicking
+        scope.select(3, 'nationality');
+
+        //Property is re-selected by clicking
+        scope.select(3, 'nationality');
+
+        expect(scope.dataProperties).toEqual(['name', 'age', 'nationality']);
+
+        //Properties are deselected by re-clicking
+        scope.select(3, 'nationality');
+        scope.select(0, 'age');
+
+        //New properties are selected by clicking
+        scope.select(2, 'salary');
+        scope.select(4, 'occupation');
+
+        expect(scope.dataProperties).toEqual(['name', 'salary', 'occupation']);
+
+    });
+
+    it('should forget all subsequent chosen properties if a selection is clicked again', function() {
+
+        //Properties are initially selected
+        scope.select(1, 'name');
+        scope.select(0, 'age');
+        scope.select(3, 'nationality');
+
+        //Property is deselected by re-clicking
+        scope.select(0, 'age');
+
+        expect(scope.dataProperties).toEqual(['name']);
+
+        //Properties are reselected
+        scope.select(0, 'age');
+        scope.select(3, 'nationality');
+
+        //Property is deselected by re-clicking
+        scope.select(1, 'name');
+
+        expect(scope.dataProperties).toEqual([]);
+    });
 
     it('should broadcast the event chartSelected with the relevant information when selectChart is called', function() {
 
