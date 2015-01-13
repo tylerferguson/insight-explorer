a15eb278da9ea9806158c76086a1b4bcf33db3be
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-01-13 12:05:50 +0000

    Reduced repeated code in dimension selection function of sidebar controller

--- app/sidebar/sidebar.controller.js
+++ app/sidebar/sidebar.controller.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-(function() {
+(function () {
 
     angular.module('insightExplorer').controller('SideCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
 
@@ -27,118 +27,83 @@
             $http.get('sidebar/charts.json'),
             $http.get('sidebar/groupingProperties.json'),
             $http.get('sidebar/dimensions.json')
-        ]).then(function(results) {
+        ]).then(function (results) {
             $scope.charts = results[0].data;
             $scope.subProps = results[1].data;
             $scope.dimensions = results[2].data;
         });
 
-        $scope.selectDimension = function(index, dimension, dataField, subProp) {
-
-            var deselectDimension = function() {
+        $scope.selectDimension = function (index, dimension, dataField, subProp) {
+            var deselectDimension = function () {
+                var indexOfDimension = $.inArray(self.dimensions[dimension].name, $scope.dataFields);
+                var dimensionGroupingProp = self.dimensions[dimension].groupingProperty;
 
                 // dimension was on a grouping property
-                if(selected[$.inArray(self.dimensions[dimension].name, $scope.dataFields)][self.dimensions[dimension].groupingProperty]) {
-
+                if (selected[indexOfDimension][dimensionGroupingProp]) {
                     //deselect previous dimension
-                    selected[$.inArray(self.dimensions[dimension].name, $scope.dataFields)][self.dimensions[dimension].groupingProperty][dimension] = '';
-                    // dimension was on a dataField
+                    selected[indexOfDimension][dimensionGroupingProp][dimension] = '';
+                // dimension was on a dataField
                 } else {
-
                     //deselect previous dimension
-                    selected[$.inArray(self.dimensions[dimension].name, $scope.dataFields)][dimension] = '';
+                    selected[indexOfDimension][dimension] = '';
                 }
             };
 
-            var selectNewDimension = function(subProp) {
-
-                if(subProp) {
+            var selectNewDimension = function (subProp) {
+                if (subProp) {
                     selected[index][subProp][dimension] = dimension;
-                    self.dimensions[dimension].name = dataField;
-                    self.dimensions[dimension].groupingProperty = subProp;
                 } else {
                     selected[index][dimension] = dimension;
-                    self.dimensions[dimension].name = dataField;
                 }
+                self.dimensions[dimension].name = dataField;
+                self.dimensions[dimension].groupingProperty = subProp;
             };
 
-            //subProp clicked
-            if(subProp) {
-
-                //Case where subProp is already selected
-                if(selected[index][subProp][dimension]) {
-
-                    selected[index][subProp][dimension] = '';
-                    self.dimensions[dimension] = {};
-                    self.numDimensions--;
-
-                //Case where dimension has been chosen elsewhere
-                } else if(!$.isEmptyObject(self.dimensions[dimension])) {
-
-                    deselectDimension();
-                    selectNewDimension(subProp);
-
-                //Case where subProp is a first time selection
-                } else {
-                    selectNewDimension(subProp);
-                    self.numDimensions++;
-                }
-            }
-            //DataField clicked
-            else {
-                //Case where dataField already selected
-                if(selected[index][dimension]) {
-
-                    selected[index][dimension] = '';
-                    self.dimensions[dimension] = {};
-                    self.numDimensions--;
-
-                //Case where dimension has been chosen elsewhere
-                } else if(!$.isEmptyObject(self.dimensions[dimension])) {
-
-                   deselectDimension();
-                   selectNewDimension();
-                //Case where dataField is a first time selection
-                } else {
-                    selectNewDimension();
-                    self.numDimensions++;
-                }
+            //Case where subProp is already selected
+            if (subProp && selected[index][subProp][dimension]) {
+                selected[index][subProp][dimension] = '';
+                self.dimensions[dimension] = {};
+                self.numDimensions--;
+            //Case where dataField already selected
+            } else if (selected[index][dimension]) {
+                selected[index][dimension] = '';
+                self.dimensions[dimension] = {};
+                self.numDimensions--;
+            //Case where dimension has been chosen elsewhere
+            } else if (!$.isEmptyObject(self.dimensions[dimension])) {
+                deselectDimension();
+                selectNewDimension(subProp);
+            //Case where dimension is a first time selection
+            } else {
+                selectNewDimension(subProp);
+                self.numDimensions++;
             }
         };
 
-        $scope.selectProperty = function(index, subProp) {
-
-            if(!selected[index]){
+        $scope.selectProperty = function (index, subProp) {
+            if (!selected[index]) {
                 selected[index] = {};
             }
 
-            if(subProp) {
-
+            if (subProp) {
                 selected[index][subProp] = $.isEmptyObject(selected[index][subProp]) ? {name: subProp} : {};
             } else {
-
                 selected[index].index = selected[index].index >= 0 ? -1 : index;
             }
         };
 
-        $scope.listOptions = function(index, event) {
-
+        $scope.listOptions = function (index, event) {
             event.stopPropagation();
-
-            selectedLists[index]  =  selectedLists[index] >= 0 ? -1 : index;
+            selectedLists[index] = selectedLists[index] >= 0 ? -1 : index;
         };
 
-        $scope.optionsListed = function(index) {
-
+        $scope.optionsListed = function (index) {
             return selectedLists[index] === index;
         };
 
-        $scope.isSelectedProperty = function(index, subProp) {
-
-            if(!$.isEmptyObject(selected[index])) {
-
+        $scope.isSelectedProperty = function (index, subProp) {
+            if (!$.isEmptyObject(selected[index])) {
                 if (subProp) {
-
                     if (!$.isEmptyObject(selected[index][subProp])) {
                         return selected[index][subProp].name === subProp;
                     }
@@ -148,29 +113,22 @@
             }
         };
 
-        $scope.isSelectedDimension = function(index, dimension, subProp) {
-
-            if(selected[index]) {
-
+        $scope.isSelectedDimension = function (index, dimension, subProp) {
+            if (selected[index]) {
                 if (subProp) {
-
-                    if ( !$.isEmptyObject(selected[index][subProp])) {
-
+                    if (!$.isEmptyObject(selected[index][subProp])) {
                         return selected[index][subProp][dimension] === dimension ? dimension : false;
                     }
                 } else {
-
                     return selected[index][dimension] === dimension ? dimension : false;
                 }
             }
         };
 
         $scope.enableByChosenProperties = function (value) {
-
             var filterByNumProperties = value.type === 'SimpleBubbleChart' ? self.numDimensions >= 3 : self.numDimensions >= 2;
 
             if (filterByNumProperties) {
-
                 var isOrdinal = !Number(self.data[0][self.dimensions.key.name]);
 
                 return isOrdinal ? value.type === 'SimpleLineChart' || value.type === 'SimpleScatterChart' : false;
@@ -180,7 +138,6 @@
         };
 
         $scope.selectChart = function (chartType) {
-
             $scope.$root.$broadcast('chartSelected', {
                 chartType: chartType,
                 data: self.data,
@@ -189,7 +146,6 @@
         };
 
         $scope.$on('dataReceived', function (event, args) {
-
             self.data = args.data;
             $scope.dataFields = Object.keys(self.data[0]);
 
